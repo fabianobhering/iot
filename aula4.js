458dc78a82adcb331ef132087db84e2c03c13baf
fabianobhering <fabianobhering@gmail.com>
2018-03-14 01:23:54 -0300

    aula4

--- aula4.js
+++ aula4.js
@@ -1,43 +1,41 @@
-/** Pós Graduação Internet das Coisas - CEFET-MG
-	Disciplina: Programação para Sistemas de Computação
-	Exemplo prático de RESTFul com NodeJS e MongoDB
+/**
+ * Pós Graduação Internet das Coisas - CEFET-MG Disciplina: Programação para
+ * Sistemas de Computação Exemplo prático de RESTFul com NodeJS e MongoDB
  */
- 
+
 /* Módulos Utilizados */
 var express = require('express'); 
-
 var cors = require('cors'); 
-
 var bodyParser = require('body-parser'); 
+var Temperatura = require('./models/temperatura'); // Modelos definidos
+var mongoose = require('mongoose');
 
-var Temperatura = require('./models/temperatura'); //Modelos definidos
+var url = "mongodb://localhost:27017/sensor";
+mongoose.connect(url);
 
-var app = express(); //Cria o app com Express
+var app = express(); // Cria o app com Express
+var router = express.Router();
 
-app.use(cors()); //liberar todos os do app acessos CORS
-
+app.use(cors()); // liberar todos os do app acessos CORS
 app.use(bodyParser.urlencoded({ 
 	extended : true
 })); 
-app.use(bodyParser.json()); //configurações do body parser
-
-
-var router = express.Router();
+app.use(bodyParser.json()); // configurações do body parser
 
 /* Rota para acompanhar as requisições */
 router.use(function(req, res, next) {
 	console.log('Entrou na rota.');
-	next(); //continua na próxima rota
+	next(); // continua na próxima rota
 });
 
-// GET /
+//GET /
 router.get('/', function(req, res) {
 	res.json({
 		message : 'API - IoT'
 	});
 });
 
-// GET /temperatura 
+//GET /temperatura
 router.route('/temperatura').get(function(req, res) {
 	Temperatura.find(function(err, temperatura) {
 		if (err)
@@ -47,20 +45,20 @@ router.route('/temperatura').get(function(req, res) {
 	});
 });
 
-// GET /temperatura/:id
+//GET /temperatura/:id
 router.route('/temperatura/:id').get(function(req, res) {
 	Temperatura.findById(req.params.id, function(error, temperatura) {
-            if(error)
-                res.send(error);
- 
-            res.json(temperatura);
-        });
-    });
+		if(error)
+			res.send(error);
 
-/*POST /temperatura  {time:"..",valor:"..."}*/
+		res.json(temperatura);
+	});
+});
+
+/* POST /temperatura {time:"..",valor:"..."} */
 router.route('/temperatura').post(function(req, res) {
 	var temperatura = new Temperatura();
-	
+
 	temperatura.time = req.body.time;
 	temperatura.valor = req.body.valor;
 
@@ -74,32 +72,32 @@ router.route('/temperatura').post(function(req, res) {
 	});
 });
 
-// PUT /temperatura/:id  {time:"..",valor:"..."}
+//PUT /temperatura/:id {time:"..",valor:"..."}
 router.route('/temperatura/:id').put(function(req, res) {
 	Temperatura.findById(req.params.id, function(error, temperatura) {
-        if(error)
-            res.send(error);
+		if(error)
+			res.send(error);
 
-        temperatura.time = req.body.time;
-    	temperatura.valor = req.body.valor;
+		temperatura.time = req.body.time;
+		temperatura.valor = req.body.valor;
 
-        temperatura.save(function(error) {
-            if(error)
-                res.send(error);
-            res.json({ message: 'Temperatura Atualizado!' });
-        });
-    });
+		temperatura.save(function(error) {
+			if(error)
+				res.send(error);
+			res.json({ message: 'Temperatura Atualizado!' });
+		});
+	});
 });
 
 //DELETE /temperatura/:id
 router.route('/temperatura/:id').delete(function(req, res) {
-    Temperatura.remove({
-    	_id: req.params.id
-    }, function(error) {
-        if(error)
-            res.send(error);
-        res.json({ message: 'Temperatura excluída com Sucesso! '});
-    });
+	Temperatura.remove({
+		_id: req.params.id
+	}, function(error) {
+		if(error)
+			res.send(error);
+		res.json({ message: 'Temperatura excluída com Sucesso! '});
+	});
 });
 
 app.use('/', router);
